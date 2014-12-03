fa0eabddb50141faca0feaf38816c0ad68e5cba9
Ahed Eid <ahedeid@gmail.com>
2014-12-03 10:11:59 +0200

    init git

---
+++ mlterm-colors.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import shutil
+import argparse
+import re
+import subprocess
+
+def get():
+    "Get colorschames"
+    if not os.path.exists(os.path.expanduser('~/.mlterm/colorschames')):
+        os.makedirs(os.path.expanduser('~/.mlterm/colorschames'))
+        if os.path.exists('/tmp/mlterm-temp'):
+            shutil.rmtree('/tmp/mlterm-temp')
+        subprocess.call('git clone --depth 1 https://github.com/Ahed91/rtlize --branch master --single-branch /tmp/mlterm-temp', shell=True)
+        data_folder = '/tmp/mlterm-temp/mlterm/colorschames' 
+    dest_folder = os.path.expanduser('~/.mlterm/colorschames')
+    for _ in os.listdir(data_folder):
+        __ = os.path.join(data_folder, _)
+        shutil.copy(__, dest_folder)
+
+def check_color(color):
+    colorschame_list = os.listdir(os.path.expanduser('~/.mlterm/colorschames'))
+    legal_color = False
+    for idx, item in enumerate(colorschame_list):
+        if color == item :
+            legal_color = True
+            return colorschame_list[idx], legal_color
+        item = item.lower()
+        if color == item :
+            legal_color = True
+            return colorschame_list[idx],  legal_color
+        item = item.upper()
+        if color == item :
+            legal_color = True
+            return colorschame_list[idx],  legal_color
+        item = item.replace('.COLOR', '')
+        if color == item :
+            legal_color = True
+            return colorschame_list[idx],  legal_color
+        item = item.lower()
+        if color == item :
+            legal_color = True
+            return colorschame_list[idx],  legal_color
+        item = item.replace(item[0], item[0].upper())
+        if color == item :
+            legal_color = True
+            return colorschame_list[idx],  legal_color
+
+    return 'none',legal_color
+
+def set_color(name):
+    colors_path = os.path.expanduser('~/.mlterm/colorschames')
+    main_path = os.path.expanduser('~/.mlterm/main')
+    color_path = os.path.join(colors_path, name)
+    with open(color_path, 'r') as f :
+        data = f.readlines()
+    with open(os.path.expanduser('~/.mlterm/color'), 'w') as f :
+        f.writelines(data[0:16])
+    
+    D = {}
+    for i,item in enumerate(data[16:23]):
+        D[i] = item
+
+    with open(main_path, 'r') as f :
+        lines = f.readlines()
+    with open(main_path, 'w') as f :
+        for line in lines:
+            if re.findall('^fg_color.*', line):
+                f.write(re.sub('^fg_color.*',D.pop(0).replace('\n','') , line))
+                continue
+            if re.findall('^bg_color.*', line):
+                f.write(re.sub('^bg_color.*',D.pop(1).replace('\n','') , line))
+                continue
+            if re.findall('^cursor_bg_color.*', line):
+                f.write(re.sub('^cursor_bg_color.*',D.pop(2).replace('\n','') , line))
+                continue
+            if re.findall('^cursor_fg_color.*', line):
+                f.write(re.sub('^cursor_fg_color.*',D.pop(3).replace('\n','') , line))
+                continue
+            if re.findall('^bd_color.*', line):
+                f.write(re.sub('^bd_color.*',D.pop(4).replace('\n','')  , line))
+                continue
+            if re.findall('^it_color.*', line):
+                f.write(re.sub('^it_color.*',D.pop(5).replace('\n','') , line))
+                continue
+            if re.findall('^ul_color.*', line):
+                f.write(re.sub('^ul_color.*',D.pop(6).replace('\n','') , line))
+                continue
+            if re.findall('^#fg_color.*', line):
+                f.write(re.sub('^#fg_color.*',D.pop(0).replace('\n','') , line))
+                continue
+            if re.findall('^#bg_color.*', line):
+                f.write(re.sub('^#bg_color.*',D.pop(1).replace('\n','') , line))
+                continue
+            if re.findall('^#cursor_bg_color.*', line):
+                f.write(re.sub('^#cursor_bg_color.*',D.pop(2).replace('\n','') , line))
+                continue
+            if re.findall('^#cursor_fg_color.*', line):
+                f.write(re.sub('^#cursor_fg_color.*',D.pop(3).replace('\n','') , line))
+                continue
+            if re.findall('^#bd_color.*', line):
+                f.write(re.sub('^#bd_color.*',D.pop(4).replace('\n','')  , line))
+                continue
+            if re.findall('^#it_color.*', line):
+                f.write(re.sub('^#it_color.*',D.pop(5).replace('\n','') , line))
+                continue
+            if re.findall('^#ul_color.*', line):
+                f.write(re.sub('^#ul_color.*',D.pop(6).replace('\n','') , line))
+                continue
+            if re.findall('^wall_picture.*', line):
+                f.write(line.replace('~', os.path.expanduser('~')))
+                continue
+            f.write(line)
+        for _ in D.keys():
+            f.write(D[_])
+
+def main():
+    """Execute"""
+    # Parser Options
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c','--colorschame', help="choose colorschame to apply on mlterm")
+    #parser.add_argument('--pic', help="set wallpaper for mlterm", action="store_true")
+    args = parser.parse_args()
+
+    if not os.path.exists(os.path.expanduser('~/.mlterm/colorschames')):
+        get()
+
+    [name,legal]=check_color(args.colorschame)
+    if not legal:
+        print 'Invalid or Unavailable Colorschame Name '
+
+    if legal:
+        set_color(name)
+
+if __name__ == '__main__':
+    main()
---
+++ mlterm/README.md
@@ -0,0 +1,30 @@
+#Mlterm config files
+
+##Main 
+
+The main file have every option and its description from mlterm manpage commented and also arranged under topics.
+
+## Color schames
+
+Color schames converted from xrdb folder in [mbadolato](https://github.com/mbadolato/iTerm2-Color-Schemes) repo you can install them using mlterm-colors.py  
+
+    ./mlterm-colors.py -c solarized-dark
+
+  - if you run it for the first time it will download the colorschames folder to ~/.mlterm/
+  - it change the contents of color file and the values of fg_color, bg_color, cursor_bg_color, cursor_fg_color, bd_color, it_color, ul_color in main file    
+
+## key
+
+key file is commented with various options from mlterm manpage
+
+## Credits
+
+[mlterm](https://bitbucket.org/arakiken/mlterm) was created by Araki Ken
+
+All the colors converted from xrdb folder in [mbadolato](https://github.com/mbadolato/iTerm2-Color-Schemes) repo except the following :
+
+    Solarized Dark was created by [altercation](https://github.com/altercation/solarized)
+
+## -- Credits to be continued
+
+
